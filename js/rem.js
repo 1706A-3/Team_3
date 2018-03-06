66cd8b09c82881fb5669c470d7748b041a0eb397
L_kun <1274353921@qq.com>
2018-03-06 10:01:21 +0800

    rem+

--- js/rem.js
+++ js/rem.js
@@ -1,4 +1,13 @@
-
-window.onload=window.onresize=function(event){
-    document.documentElement.style.fontSize=10*(document.documentElement.clientWidth/320)+'px';
-}
+(function(){
+    size();
+    window.onresize=function(){
+        size();
+    }
+    function size(){
+        var winW=document.documentElement.clientWidth||document.body.clientWidth;
+        document.documentElement.style.fontSize=(winW/320)*10+"px";
+    }
+})()
+// 1rem =10px
+// 屏幕最小支持320px
+// 最小字体10px
